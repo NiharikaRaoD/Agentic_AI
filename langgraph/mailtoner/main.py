09b6a0ecb17775f0ac481675fb69ced3d9278625
NiharikaRaoD <niharika.dharmana@outlook.com>
2025-10-31 22:01:19 -0500

    Added Exercise to change Email tone

--- langgraph/mailtoner/main.py
+++ langgraph/mailtoner/main.py
@@ -1,10 +1,12 @@
-from dotenv import load_dotenv
-import os
+from mail_tone import graph, Email
 
 def main():
-    print(os.getenv('LANGSMITH_TEST_KEY', 'No key found'))
-    load_dotenv()
-    print(os.getenv('LANGSMITH_TEST_KEY', 'No key found'))
+    state = Email(
+        Draft_Email='I will be out of office from Nov 3rd and will be returning on Nov 10th. I cannot access my emails or messages during this time. Reach out on my mobile xxx-xxx-xxxx if you need anything. If tehre any any critical things, contact my Manager',
+        Email_Tone='Formal'
+    )
+    response = graph.invoke(state)
+    print(response['Final_Email'])
 
 
 if __name__ == "__main__":
