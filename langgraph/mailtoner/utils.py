2409e5da3674ea9a78ea133a73ec31c5a4714cd8
NiharikaRaoD <niharika.dharmana@outlook.com>
2025-11-08 18:24:54 -0600

    MessageState Tutorial

--- langgraph/mailtoner/utils.py
+++ langgraph/mailtoner/utils.py
@@ -1,3 +1,16 @@
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.language_models import BaseChatModel
+from langchain_core.prompts import ChatPromptTemplate
+import os
+
 def draw_graph(graph):
     from IPython.display import Image, display
     display(Image(graph.get_graph().draw_mermaid_png()))
+
+def get_llm() -> BaseChatModel:
+    load_dotenv()
+    model_name = os.getenv('MODEL_NAME')
+    model_provider = os.getenv('MODEL_PROVIDER')
+    llm = init_chat_model(model=model_name, model_provider=model_provider)
+    return llm
